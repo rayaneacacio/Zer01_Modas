a9f9d5e341f768ac993d07110851518baf460dda
rayaneacacio <rray74307@gmail.com>
2023-09-12 16:17:34 -0300

    criacao do componente button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,8 @@
 import { Container } from "./style";
 
 export function Button({ title, $background_color, ...rest }) {
-  // console.log(document.querySelector("#button"))
-
   return (
-    <Container {...rest}>
+    <Container $background_color={ $background_color } {...rest}>
       { title }
     </Container>
   )
--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
+  background: ${({ $background_color }) => $background_color};
+  color: ${({ theme }) => theme.COLORS.BLACK};
+  padding: 0.5rem 1rem;
 `;
