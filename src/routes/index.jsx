79a8ee8d1870a560b84511915c83e9b27c3bed8a
rayaneacacio <rray74307@gmail.com>
2023-10-09 13:02:40 -0300

    corrigindo problema com redimensionamento de vh na versao mobile da aplicacao

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,11 @@ import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 import { PayRoutes } from "./pay.routes";
 
+import { resizeLayout } from "../scripts/resize-layout";
+
 export function Routes() {
+  resizeLayout();
+
   return (
     <BrowserRouter>
       <AuthRoutes />
