c0ef20487c1ed1b62696885a4dcc7c69512e3242
rayaneacacio <rray74307@gmail.com>
2023-10-30 11:31:09 -0300

    update routes

--- zer01modas-frontend/src/routes/app.routes.jsx
+++ zer01modas-frontend/src/routes/app.routes.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
-import { Menu } from "../pages/Menu";
 import { Catalog } from "../pages/Catalog";
 import { Outfit } from "../pages/Outfit";
 import { Favorites } from "../pages/Favorites";
@@ -12,7 +11,6 @@ export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
-      <Route path="/menu" element={ <Menu /> } />
       <Route path="/catalog" element={ <Catalog /> } />
       <Route path="/outfit" element={ <Outfit /> } />
       <Route path="/favorites" element={ <Favorites /> } />
