61a6dfa26dc09b89450ba65fe636a4d2531b1bb3
rayaneacacio <rray74307@gmail.com>
2023-11-10 11:21:21 -0300

    construindo hook products attributes

--- src/hooks/productAttributes.jsx
+++ src/hooks/productAttributes.jsx
@@ -38,8 +38,35 @@ function ProductAttributesProvider({ children }) {
     await api.post("/products_sizes", { product_id, color_id, sizes });
   }
 
+  async function allColorsOfProduct(product_id) {
+    try {
+      const response = await api.get(`/products_colors/index_colors?product_id=${ product_id }`);
+      return response.data;
+
+    } catch(error) {
+      if(error) {
+        console.log(error);
+      } else {
+        console.log("cores nao encontradas");
+      }
+    }
+  }
+
+  async function deleteImgs(product_id) {
+    try {
+      await api.post("/products_images/delete", { product_id });
+
+    } catch(error) {
+      if(error) {
+        console.error(error);
+      } else {
+        console.log("erro ao deletar imagens");
+      }
+    }
+  }
+
   return (
-    <productAttributesContext.Provider value={{ sectionsList, saveSectionsStorage, AddAttributes }}>
+    <productAttributesContext.Provider value={{ sectionsList, saveSectionsStorage, AddAttributes, allColorsOfProduct, deleteImgs }}>
       { children }
     </productAttributesContext.Provider>
   )
