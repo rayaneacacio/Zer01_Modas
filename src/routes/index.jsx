f1ced1f9b654b718c5d16d62cdc04857b0ecde08
rayaneacacio <rray74307@gmail.com>
2023-11-10 19:55:14 -0300

    update routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,41 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from "./app.routes";
-import { PayRoutes } from "./pay.routes";
+import { useAuth } from "../hooks/auth";
+
+import { Home } from "../pages/Home";
+import { Catalog } from "../pages/Catalog";
+import { Outfit } from "../pages/Outfit";
+import { Favorites } from "../pages/Favorites";
+import { New } from "../pages/New";
+import { Edit } from "../pages/Edit";
+import { Signature } from "../pages/Signature";
+import { ShoppingCart } from "../pages/ShoppingCart";
+import { Payment } from "../pages/Payment";
 import NotFound404 from "../pages/404";
 
 export function Zer01ModasRoutes() {
+  const { isAdmin } = useAuth();
+
   return (
     <BrowserRouter>
-      <AuthRoutes />
-      <AppRoutes />
-      <PayRoutes />
       <Routes>
+        <Route path="/login" element={ <Signature /> } />
+        <Route path="/" element={ <Home /> } />
+        <Route path="/catalog" element={ <Catalog /> } />
+        <Route path="/outfit" element={ <Outfit /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+
+        {
+          isAdmin && <Route path="/new" element={ <New /> } />
+        }
+        
+        {
+          isAdmin && <Route path="/edit" element={ <Edit /> } />
+        }
+
+        <Route path="/shopping-cart" element={ <ShoppingCart /> } />
+        <Route path="/payment" element={ <Payment /> } />
+
         <Route path="*" element={ <NotFound404 /> } />
       </Routes>
     </BrowserRouter>
