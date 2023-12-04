0df4f439071adeb5b83a5fb438df06cd7c6b279b
rayaneacacio <rray74307@gmail.com>
2023-12-04 09:44:46 -0300

    update home e banners de promocoes

--- src/components/Promotion-card/index.jsx
+++ src/components/Promotion-card/index.jsx
@@ -5,7 +5,6 @@ export function PromotionalCard({ img_png, title, promotion }) {
     <Container>
       <img src={ img_png } alt="" />
       <div>
-        <h2> MODA <strong> { title } </strong> </h2>
         <h2> { promotion } OFF </h2>
       </div>
     </Container>
--- src/components/Promotion-card/style.js
+++ src/components/Promotion-card/style.js
@@ -8,10 +8,9 @@ export const Container = styled.button`
   }
 
   > div {
-    background: rgba(0, 0, 0, 0.85);
     color: ${({ theme }) => theme.COLORS.WHITE};
     width: 15rem;
-    height: 6rem;
+    height: 100%;
     position: absolute;
     bottom: 0;
     display: flex;
@@ -28,13 +27,12 @@ export const Container = styled.button`
 
   @media(min-width: 1000px) {
     > img {
-      width: 24rem;
+      width: 26rem;
       height: 37rem;
     }
 
     > div {
-      width: 24rem;
-      height: 12rem;
+      width: 26rem;
       
       h2 {
         font-size: 1.8rem;
